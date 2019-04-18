89dfda3f38bbfb6e6e33320b76898e751a983ec4
Stephen Underwood <41934385+stephenunder@users.noreply.github.com>
2019-04-18 13:03:04 -0500

    fix onSubmitSignIn func

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "./Signin.css";
 
-class Signin extends React.Component {
+class Signin extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -28,10 +28,10 @@ class Signin extends React.Component {
       })
     })
       .then(response => response.json())
-      .then(user => {
-        if (user.id) {
-          this.props.loadUser(user)
-          this.props.onRouteChange('home');
+      .then(data => {
+        if (data && data.success === "true") {
+          this.props.loadUser(data)
+          this.props.onRouteChange("home");
         }
       })
   }
