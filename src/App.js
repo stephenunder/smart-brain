695c61419a0c5a3bc0c0ded3821763fe0be34a3c
Stephen Underwood <41934385+stephenunder@users.noreply.github.com>
2019-04-18 16:48:07 -0500

    fix profile update

--- src/App.js
+++ src/App.js
@@ -62,7 +62,20 @@ class App extends Component {
         .then(resp => resp.json())
         .then(data => {
           if (data && data.id) {
-            console.log("success we need to get user profile")
+            fetch(`http://localhost:3000/profile/${data.id}`, {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                "Authorization": token
+              }
+            })
+            .then(resp => resp.json())
+            .then(user => {
+              if (user && user.email) {
+                this.loadUser(user)
+                this.onRouteChange("home");
+              }
+            })
           }
         })
         .catch(console.log)
@@ -80,22 +93,27 @@ class App extends Component {
   }
 
   calculateFaceLocations = (data) => {
-    const image = document.getElementById("inputimage");
-    const width = Number(image.width);
-    const height = Number(image.height);
-    return data.outputs[0].data.regions.map(face => {
-      const clarifaiFace = face.region_info.bounding_box;
-      return {
-        leftCol: clarifaiFace.left_col * width,
-        topRow: clarifaiFace.top_row * height,
-        rightCol: width - (clarifaiFace.right_col * width),
-        bottomRow: height - (clarifaiFace.bottom_row * height)
-      }
-    })
+    if (data && data.outputs) {
+      const image = document.getElementById("inputimage");
+      const width = Number(image.width);
+      const height = Number(image.height);
+      return data.outputs[0].data.regions.map(face => {
+        const clarifaiFace = face.region_info.bounding_box;
+        return {
+          leftCol: clarifaiFace.left_col * width,
+          topRow: clarifaiFace.top_row * height,
+          rightCol: width - (clarifaiFace.right_col * width),
+          bottomRow: height - (clarifaiFace.bottom_row * height)
+        }
+      })
+    }
+    return;
   }
 
   displayFaceBoxes = (boxes) => {
-    this.setState({boxes: boxes});
+    if (boxes) {
+      this.setState({boxes: boxes});
+    }
   }
 
   onInputChange = (event) => {
@@ -106,7 +124,10 @@ class App extends Component {
     this.setState({imageUrl: this.state.input});
       fetch("http://localhost:3000/imageurl", {
         method: 'post',
-        headers: {'Content-Type': "application/json"},
+        headers: {
+          'Content-Type': "application/json",
+          "Authorization": window.sessionStorage.getItem("token")
+                 },
         body: JSON.stringify({
           input: this.state.input
         })
@@ -116,7 +137,10 @@ class App extends Component {
         if (response) {
           fetch("http://localhost:3000/image", {
             method: "put",
-            headers: {"Content-Type": "application/json"},
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": window.sessionStorage.getItem("token")
+            },
             body: JSON.stringify({
               id: this.state.user.id
             })
