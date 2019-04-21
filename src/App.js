c9cacc7b4b3b7e1934c6ff7ca3292201c86b7686
Stephen Underwood <41934385+stephenunder@users.noreply.github.com>
2019-04-20 23:53:57 -0500

    add age and pet to loadUser setState

--- src/App.js
+++ src/App.js
@@ -88,7 +88,9 @@ class App extends Component {
       name: data.name,
       email: data.email,
       entries: data.entries,
-      joined: data.joined
+      joined: data.joined,
+      age: data.age,
+      pet: data.pet
     }})
   }
 
